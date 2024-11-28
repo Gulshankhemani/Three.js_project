25f215ef7333607d54fd8da075a10eb9a2815022
Gulshankhemani <gulshankhemani2@gamil.com>
2024-11-28 13:13:04 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,10 @@
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Sparkles } from "@react-three/drei";
-
-const RotatingCube = () => {
-  const meshRef = useRef();
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef} position={[0, 0, 0]}>
-      <cylinderGeometry args={[1, 1, 1]} />
-      <meshLambertMaterial color="#468585" emissive="#468585" />
-      <Sparkles count={100} scale={1} size={6} speed={0.002} color="orange" />
-    </mesh>
-  );
-};
+import React from 'react'
 
 const App = () => {
   return (
-    <Canvas
-      camera={{ position: [0, 0, 5] }}
-      style={{
-        height: "100vh",
-        width: "100lvw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <OrbitControls enableZoom enablePan enableRotate />
-      <directionalLight position={[1, 1, 1]} intensity={10} color="#9cdba6" />
-      <color attach="background" args={["#f0f0f0"]} />
-      <RotatingCube />
-    </Canvas>
-  );
-};
-
-export default App;
+    <main className='max-w-7xl mx-auto'>
+      <h1 className='text-2xl text-white underline'>My Three.js App</h1>
+    </main>
+  )
+}
+export default App
